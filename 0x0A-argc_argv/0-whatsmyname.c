1f79c994c9a4c783a7dc42b839d99b5fb276b1b4
Vicktorh11 <prinzvee11@gmail.com>
2022-11-17 00:37:03 -0800

    print its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,8 +8,8 @@
   * Return: Always 0
   */
 
-int main(int argc _attribute_((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	print("%s\n", argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
